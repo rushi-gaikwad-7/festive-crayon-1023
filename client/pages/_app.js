a00acea6b082413063fbae43bafa5ee251ca83ff
rushig777 <gaikwadrushi777@gmail.com>
2022-07-20 22:19:15 +0530

    ..added pack

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import "bootstrap/dist/css/bootstrap.css";
+import Navbar from '../components/Header/Navbar';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return( 
+    <>
+     <Navbar/>
+  <Component {...pageProps} />
+    </>
+ 
+  )
 }
 
 export default MyApp
