0c7d0c3ac61cd96786b86c21b72d5454870541dd
nimishshinde <mayurshinde2310@gmail.com>
2022-07-21 13:37:11 +0530

    update in index.js

--- server/src/controller/home.controller.js
+++ server/src/controller/home.controller.js
@@ -2,7 +2,7 @@ const Home = require("../model/home.model")
 
 const getHomeData = async (type)=>{
  console.log(type)
-let data = await Home.find({gender : type})
+let data = await Home.findOne({gender : type})
 if(data.length<1){
     return "error"
 }
