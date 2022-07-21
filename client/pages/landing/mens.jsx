e3314d8de2b9db79c1d215b62471b1ca21c46572
nimishshinde <mayurshinde2310@gmail.com>
2022-07-21 23:47:01 +0530

    Update mens.jsx

--- client/pages/landing/mens.jsx
+++ client/pages/landing/mens.jsx
@@ -41,7 +41,11 @@ export default function Landing({ data }) {
   };
   return (
     <>
-    <div className={styles.blue}><h2 className={styles.bluestrip}>Free shipping on ALL orders + Flat ₹200 off on ₹1999. Code: MAX200</h2></div>
+      <div className={styles.blue}>
+        <h2 className={styles.bluestrip}>
+          Free shipping on ALL orders + Flat ₹200 off on ₹1999. Code: MAX200
+        </h2>
+      </div>
       <div className={styles.mainland}>
         <Slider {...settings} className={styles.slimain}>
           {data.sliderimages.map((el) => {
@@ -59,7 +63,7 @@ export default function Landing({ data }) {
             );
           })}
         </Slider>
-      <div className={styles.off}>
+        <div className={styles.off}>
           <Image
             className={styles.imageoff}
             loader={myLoader}
@@ -84,137 +88,141 @@ export default function Landing({ data }) {
                 );
               })}
             </div>
-         <div className={styles.cusfav}>
-            <h2 className={styles.landtitles}>{data.CustomerFav.title}</h2>
-            <div>
+            <div className={styles.cusfav}>
+              <h2 className={styles.landtitles}>{data.CustomerFav.title}</h2>
+              <div>
+                <Image
+                  className={styles.cusimg}
+                  loader={myLoader}
+                  src={data.CustomerFav.url}
+                  width={1232}
+                  height={485}
+                />
+              </div>
+            </div>
+            <div className={styles.bycats}>
+              <h2 className={styles.landtitles}>{data.shopbycat.title}</h2>
+              <div className={styles.catcontain}>
+                {data.shopbycat.catarr.map((el) => {
+                  return (
+                    <div className={styles.catelem}>
+                      <Image
+                        className={styles.catimgs}
+                        loader={myLoader}
+                        src={el.img}
+                        width={288}
+                        height={288}
+                      />
+                      <h2 className={styles.cattitle}>{el.title}</h2>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div className={styles.budget}>
+              <h2 className={styles.landtitles}>{data.budget.title}</h2>
+              <div className={styles.buds}>
+                {data.budget.budgt.map((el) => {
+                  return (
+                    <Image
+                      className={styles.budimgs}
+                      loader={myLoader}
+                      src={el}
+                      alt="budgetimgs"
+                      width={400}
+                      height={200}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+            {data.youthstore && (
+              <div className={styles.youthstore}>
+                <h2 className={styles.landtitles}>{data.youthstore.title}</h2>
+                <Image
+                  className={styles.youthimg}
+                  loader={myLoader}
+                  src={data.youthstore.youth}
+                  alt="budgetimgs"
+                  width={1232}
+                  height={485}
+                />
+              </div>
+            )}
+            <div className={styles.trends}>
+              <h2 className={styles.landtitles}>{data.trends.title}</h2>
+              <div className={styles.trendgrid}>
+                {data.trends.trendimgs.map((el) => {
+                  return (
+                    <Image
+                      className={styles.trendimg}
+                      loader={myLoader}
+                      src={el}
+                      alt="trendimgs"
+                      width={600}
+                      height={600}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+            {data.topstores && (
+              <div className={styles.topstore}>
+                <h2 className={styles.landtitles}>{data.topstores.title}</h2>
+                <div className={styles.tops}>
+                  {data.topstores.top.map((el) => {
+                    return (
+                      <Image
+                        className={styles.storeimg}
+                        loader={myLoader}
+                        src={el}
+                        alt="topimgs"
+                        width={600}
+                        height={600}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+            <div className={styles.view}>
               <Image
-                className={styles.cusimg}
+                className={styles.viewimg}
                 loader={myLoader}
-                src={data.CustomerFav.url}
+                src={data.viewall}
+                alt="topimgs"
                 width={1232}
-                height={485}
+                height={160}
               />
             </div>
-            </div> 
-          <div className={styles.bycats}>
-            <h2 className={styles.landtitles}>{data.shopbycat.title}</h2>
-            <div className={styles.catcontain}>
-              {data.shopbycat.catarr.map((el) => {
-                return (
-                  <div className={styles.catelem}>
-                    <Image
-                      className={styles.catimgs}
-                      loader={myLoader}
-                      src={el.img}
-                      width={288}
-                      height={288}
-                    />
-                    <h2 className={styles.cattitle}>{el.title}</h2>
-                  </div>
-                );
-              })}
-            </div>
           </div>
-          <div className={styles.budget}>
-            <h2 className={styles.landtitles}>{data.budget.title}</h2>
-            <div className={styles.buds}>
-              {data.budget.budgt.map((el) => {
-                return (
-                  <Image
-                    className={styles.budimgs}
-                    loader={myLoader}
-                    src={el}
-                    alt="budgetimgs"
-                    width={400}
-                    height={200}
-                  />
-                );
-              })}
-            </div>
-          </div>
-         {data.youthstore&&<div className={styles.youthstore}>
-            <h2 className={styles.landtitles}>{data.youthstore.title}</h2>
-            <Image
-              className={styles.youthimg}
-              loader={myLoader}
-              src={data.youthstore.youth}
-              alt="budgetimgs"
-              width={1232}
-              height={485}
-            />
-          </div>}
-          <div className={styles.trends}>
-            <h2 className={styles.landtitles}>{data.trends.title}</h2>
-            <div className={styles.trendgrid}>
-              {data.trends.trendimgs.map((el) => {
-                return (
-                  <Image
-                    className={styles.trendimg}
-                    loader={myLoader}
-                    src={el}
-                    alt="trendimgs"
-                    width={600}
-                    height={600}
-                  />
-                );
-              })}
-            </div>
-          </div>
-          {data.topstores&&<div className={styles.topstore}>
-            <h2 className={styles.landtitles}>{data.topstores.title}</h2>
-            <div className={styles.tops}>
-              {data.topstores.top.map((el) => {
-                return (
-                  <Image
-                    className={styles.storeimg}
-                    loader={myLoader}
-                    src={el}
-                    alt="topimgs"
-                    width={600}
-                    height={600}
-                  />
-                );
-              })}
-            </div>
-          </div>}
-          <div className={styles.view}>
+          <div className={styles.giftzone}>
+            <h2 className={styles.landtitles}>Gifting Zone</h2>
             <Image
               className={styles.viewimg}
               loader={myLoader}
-              src={data.viewall}
-              alt="topimgs"
+              src={
+                "https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/MAX-Friday/MAX2.O/desktop-modularblock14One-Men-26Oct2021.jpg"
+              }
+              alt="giftimgs"
               width={1232}
-              height={160}
+              height={250}
+            />
+          </div>
+          <div className={styles.connect}>
+            <h2 className={styles.landtitles}>Connect With Us</h2>
+            <Image
+              className={styles.viewimg}
+              loader={myLoader}
+              src={
+                "https://lmsin.net/cdn-cgi/image/w=1232,q=60,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-modularblock8-oneBythree-A-Women-21Oct2020.jpg"
+              }
+              alt="connectimg"
+              width={1232}
+              height={280}
             />
           </div>
         </div>
-        <div className={styles.giftzone}>
-          <h2 className={styles.landtitles}>Gifting Zone</h2>
-          <Image
-            className={styles.viewimg}
-            loader={myLoader}
-            src={
-              "https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/MAX-Friday/MAX2.O/desktop-modularblock14One-Men-26Oct2021.jpg"
-            }
-            alt="giftimgs"
-            width={1232}
-            height={250}
-          />
-        </div>
-        <div className={styles.connect}>
-          <h2 className={styles.landtitles}>Connect With Us</h2>
-          <Image
-            className={styles.viewimg}
-            loader={myLoader}
-            src={
-              "https://lmsin.net/cdn-cgi/image/w=1232,q=60,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/desktop-department-modularblock8-oneBythree-A-Women-21Oct2020.jpg"
-            }
-            alt="connectimg"
-            width={1232}
-            height={280}
-          />
-        </div>
-      </div>
       </div>
       <div style={{ border: "0.5px solid #ecedeb" }}></div>
     </>
@@ -224,8 +232,5 @@ export default function Landing({ data }) {
 export async function getServerSideProps() {
   const res = await fetch(`http://localhost:8080/home/get/mens`);
   let data = await res.json();
-  console.log(data);
-  return { props: { data } };
+  return { props: { data } }
 }
-
-
