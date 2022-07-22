93150bd8787f6110f3fec3342c2e356ade1291cc
rushig777 <gaikwadrushi777@gmail.com>
2022-07-22 10:56:57 +0530

    last push of morning

--- server/src/routes/Products.routes.js
+++ server/src/routes/Products.routes.js
@@ -26,46 +26,6 @@ ProductRouter.get('/:id', async (req, res) => {
 
 })
 
-// ProductRouter.post('/new', async (req, res) => {
-
-//     const {
-//         Title,
-//         Price,
-//         Images,
-//         Sizes,
-//         Country,
-//         Category,
-//         Color,
-//         Type,
-//         Status,
-//         Stock,
-//         Gender
-//     } = req.body
-//     const product = new Product({
-//         Title,
-//         Price,
-//         Images,
-//         Sizes,
-//         Country,
-//         Category,
-//         Color,
-//         Type,
-//         Status,
-//         Stock,
-//         Gender
-//     })
-//     product.save((err, success) => {
-//         if (success) {
-//             res.status(201).send({
-//                 massage: "new products created",
-//                 product
-//             })
-//         } else {
-//             res.status(401).send({
-//                 massage: "error occurred",
-//                 err
-//             })
-//         }
 
 ProductRouter.get('/',async(req,res)=>{
 
