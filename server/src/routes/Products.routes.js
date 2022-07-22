cd440d9aa2cfffbc64ab76d01b83518f7579feff
rushig777 <gaikwadrushi777@gmail.com>
2022-07-22 10:46:39 +0530

    ..

--- server/src/routes/Products.routes.js
+++ server/src/routes/Products.routes.js
@@ -24,55 +24,48 @@ ProductRouter.get('/:id', async (req, res) => {
         res.status(401).send(err)
     }
 
-const {Router}=require("express");
-const Category = require("../model/category");
-const Product = require("../model/products");
-
-
-
-
 })
 
-ProductRouter.post('/new', async (req, res) => {
+// ProductRouter.post('/new', async (req, res) => {
 
-    const {
-        Title,
-        Price,
-        Images,
-        Sizes,
-        Country,
-        Category,
-        Color,
-        Type,
-        Status,
-        Stock,
-        Gender
-    } = req.body
-    const product = new Product({
-        Title,
-        Price,
-        Images,
-        Sizes,
-        Country,
-        Category,
-        Color,
-        Type,
-        Status,
-        Stock,
-        Gender
-    })
-    product.save((err, success) => {
-        if (success) {
-            res.status(201).send({
-                massage: "new products created",
-                product
-            })
-        } else {
-            res.status(401).send({
-                massage: "error occurred",
-                err
-            })
-        }
+//     const {
+//         Title,
+//         Price,
+//         Images,
+//         Sizes,
+//         Country,
+//         Category,
+//         Color,
+//         Type,
+//         Status,
+//         Stock,
+//         Gender
+//     } = req.body
+//     const product = new Product({
+//         Title,
+//         Price,
+//         Images,
+//         Sizes,
+//         Country,
+//         Category,
+//         Color,
+//         Type,
+//         Status,
+//         Stock,
+//         Gender
+//     })
+//     product.save((err, success) => {
+//         if (success) {
+//             res.status(201).send({
+//                 massage: "new products created",
+//                 product
+//             })
+//         } else {
+//             res.status(401).send({
+//                 massage: "error occurred",
+//                 err
+//             })
+//         }
 
 ProductRouter.get('/',async(req,res)=>{
 
@@ -103,6 +96,4 @@ ProductRouter.post('/new',async(req,res)=>{
 })
 
 
-
-module.exports = ProductRouter;
-
+module.exports = ProductRouter
