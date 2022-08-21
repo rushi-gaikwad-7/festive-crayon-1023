fd06983a4eb66deab0c84df24ca008d693452f1f
nimishshinde <mayurshinde2310@gmail.com>
2022-08-21 15:45:30 +0530

    Update auth.controller.js

--- server/src/controller/auth.controller.js
+++ server/src/controller/auth.controller.js
@@ -33,6 +33,7 @@ const authController = {
         httpOnly: true,
         path: "/auth/refresh_token",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        secure: true,
       });
 
       res.json({
