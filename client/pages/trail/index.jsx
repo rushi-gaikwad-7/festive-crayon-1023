12644854221e6f66aa9b991f1659c10007e79b6a
rushig777 <gaikwadrushi777@gmail.com>
2022-08-21 19:49:44 +0530

    .

--- client/pages/trail/index.jsx
+++ client/pages/trail/index.jsx
@@ -1,22 +1,22 @@
-// import axios from "axios"
-// import {data} from "../"
+import axios from "axios"
+import {data} from "../"
 
-// export default function Prod(){
-//     let handlecart = async (id)=>{
-//         let res = await axios.post(`/home/post/${id}`)
-//     }
-//     return (
-//         <div>
-//             {data.map((el)=>{
-//               return  <div>
-//                 <img src={el.Images[0]}/>
-//                 <button onClick={()=>{
-//                  handlecart(el._id)   
-//                 }}>add to cart</button>
-//                 </div>
-//             })}
-//         </div>
-//     )
-// }
+export default function Prod(){
+    let handlecart = async (id)=>{
+        let res = await axios.post(`/home/post/${id}`)
+    }
+    return (
+        <div>
+            {data.map((el)=>{
+              return  <div>
+                <img src={el.Images[0]}/>
+                <button onClick={()=>{
+                 handlecart(el._id)   
+                }}>add to cart</button>
+                </div>
+            })}
+        </div>
+    )
+}
 
 
